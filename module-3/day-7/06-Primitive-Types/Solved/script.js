f17ad599139c3f9ae54291c6e494d4e7f36b4d13
Christian Areas <me@areas.me>
2022-09-20 21:31:57 -0700

    Completed Day 7 activities.

--- module-3/day-7/06-Primitive-Types/Solved/script.js
+++ module-3/day-7/06-Primitive-Types/Solved/script.js
@@ -5,19 +5,33 @@ var four = 64.55;
 var five = "Howdy!";
 var six;
 
-// Insert comments to explain what each console log below will log to the console
-console.log(typeof one); 
-console.log(typeof two); 
-console.log(typeof three); 
-console.log(typeof four); 
+// the value type for one (number)
+console.log(typeof one);
+
+// the value type for two (string)
+console.log(typeof two);
+
+// the value type for three (boolean)
+console.log(typeof three);
+
+// the value type for four (number)
+console.log(typeof four);
+
+// the value type for five (string)
 console.log(typeof five);
+
+// the value type for six (undefined)
 console.log(typeof six);
 
 four = "Hello!";
 five = false;
 six = 23;
 
-// Insert comments to explain what each console log below will log to the console
+// the new value type for four (string)
 console.log(typeof four);
+
+// the new value type for five (boolean)
 console.log(typeof five);
+
+// the new value type for six (number)
 console.log(typeof six);
--- module-3/day-7/10-Conditional-Statements/Solved/script.js
+++ module-3/day-7/10-Conditional-Statements/Solved/script.js
@@ -1,6 +1,17 @@
 // Change the values and operators below to test your algorithm meets all conditions
-var x = 50;
-var expression1 = (x < 25);
-var expression2 = (x > 50);
+var x = 25;
+var expression1 = (x = 25);
+var expression2 = (x >= 25);
 
-// Write Your JavaScript Code Here
+// conditional
+if (expression1 && expression2) {
+  console.log("True ✅ True ✅");
+} else if (expression1 && !expression2) {
+  console.log("True ✅ False ❌");
+} else if (!expression1 && expression2) {
+  console.log("False ❌ True ✅");
+} else if (!expression1 && !expression2) {
+  console.log("False ❌ False ❌");
+} else {
+  console.log("Oops, something went wrong.")
+}
